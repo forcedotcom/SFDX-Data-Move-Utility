aca5672bb5a9e95d9fe0407c39599b03cfeb7b6e
Haim Knokh <haim.knokh@gmail.com>
2021-10-21 00:32:52 +0300

    Fixed resources

--- src/modules/models/script_models/scriptObject.ts
+++ src/modules/models/script_models/scriptObject.ts
@@ -744,7 +744,7 @@ export default class ScriptObject {
         incorrect = ___getIncorrectPolymorphicFields(this.sourceSObjectDescribe, incorrectDeclarations);
 
         incorrectDeclarations.forEach(fieldName => {
-            this.script.logger.warn(RESOURCES.fieldIsNotOfPolymorphicType, this.name, fieldName);
+            this.script.logger.infoVerbose(RESOURCES.fieldIsNotOfPolymorphicType, this.name, fieldName);
         });
 
         if (incorrect.length > 0) {
@@ -757,7 +757,7 @@ export default class ScriptObject {
                 if (incorrect.indexOf(fieldName) < 0) {
                     this.parsedQuery.fields.push(getComposedField(fieldName));
                 } else {
-                    this.script.logger.warn(RESOURCES.fieldMissingPolymorphicDeclaration, this.name, fieldName, fieldName);
+                    this.script.logger.infoVerbose(RESOURCES.fieldMissingPolymorphicDeclaration, this.name, fieldName, fieldName);
                 }
             });
             // Create new query string
